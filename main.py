595e61727daeb58dee77881f1c3e8f784ced486d
toz00 <44646289+toz00@users.noreply.github.com>
2020-12-14 16:02:38 -0300

    v2

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import requests
 import pandas as pd
+import numpy as np
 from bs4 import BeautifulSoup
 import re
 from selenium import webdriver
 import time
 from selenium.webdriver.support.ui import Select
+from prettytable import PrettyTable
+from statistics import mean
+op = webdriver.ChromeOptions()
+op.add_argument('headless')
+driver = webdriver.Chrome('./chromedriver',options=op)
 
-driver = webdriver.Chrome('./chromedriver.exe')
 headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 5.1.1; SM-G928X Build/LMY47X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.83 Mobile Safari/537.36'}
 
-supermercados = ["Tienda Inglesa","Disco","geant","Tata","devoto"]
-frutaverdura =["Banana", "Tomate", "Naranja", "Palta", "Moron","Palta"]
+table = PrettyTable()
+
+supermercados = ["T. Inglesa","Disco","Geant","Tata","Devoto"]
+frutaverdura =["Lugar","Banana", "Tomate", "Naranja", "Palta", "Moron","Palta"]
 
 
 
 #banana Agent
+print ("start bananas")
 banana=[]
 
 #tiendainglesa
@@ -47,7 +55,6 @@ soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 results = soup.findAll(class_='precio-por-unidad')
 
-
 for i in range(len(results)):
     resultstr= str(results[i])
 
@@ -79,7 +86,8 @@ driver.find_element_by_id("state").click()
 time.sleep(0.5)
 driver.find_element_by_id("react-select-2-option-1").click()
 time.sleep(1)
-submit_button = driver.find_element_by_class_name("styles__Button-sc-15ln7eo-0")
+submit_button = driver.find_element_by_class_name('bzYhsb')
+time.sleep(0.5)
 try:
     submit_button.click()
 except:
@@ -88,12 +96,12 @@ time.sleep(4)
 soup = BeautifulSoup(driver.page_source,'html.parser')
 
 
-results = soup.findAll(class_='styles__PPUMPrice-sc-12qeqj4-0')
+results = soup.findAll(class_='csCkcm')
 
 
-for i in range(2):
-    resultstr= str(results[i])
-    banana[-1].append(int(resultstr[resultstr.find("$")+1:resultstr.find(".00 x kg")]))
+
+resultstr= str(results)
+banana[-1].append(int(resultstr[resultstr.find("$")+1:resultstr.find(".00")]))
 
 
 
@@ -121,10 +129,14 @@ for i in range(len(results)):
 
 
 #Tomate Agent
+print ("start tomate")
 tomate=[]
 
 #tiendainglesa
 tomate.append([])
+tomate[-1].append(80)
+tomate[-1].append(80)
+"""
 URL = 'https://www.tiendainglesa.com.uy/Categoria/Frescos/Verduras/busqueda?0,0,tomate,1894,196,0,rel,%5B%22Otras%22%5D,false,%5B%5D,%5B%5D,'
 driver.get(URL)
 time.sleep(5)
@@ -132,15 +144,15 @@ soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 results = soup.findAll(class_='ProductPrice')
 
-for i in range(len(results)):
+for i in range(results):
     resultstr= str(results[i])
     tomate[-1].append(int(resultstr[resultstr.find("$")+1:resultstr.find("$")+4]))
-
+"""
 
 
 #disco
 tomate.append([])
-URL = 'https://www.disco.com.uy/frescos/frutas-y-verduras/verduras/tomate?PS=20&sc=4'
+URL = 'https://www.disco.com.uy/frescos/frutas-y-verduras/verduras/tomate?PS=20&sc=4&O=OrderByNameASC'
 driver.get(URL)
 time.sleep(3)
 submit_button = driver.find_element_by_id("btnConfirmaSucursal")
@@ -154,7 +166,7 @@ soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 results = soup.findAll(class_='precio-por-unidad')
 
-del results[3]
+del results[1]
 
 for i in range(len(results)):
     resultstr= str(results[i])
@@ -182,16 +194,16 @@ tomate.append([])
 URL = 'https://www.tata.com.uy/buscar?text=tomate&whitelabelRFlag=1'
 #page = requests.get(URL,headers=headers)
 driver.get(URL)
-time.sleep(3)
-driver.find_element_by_id("state").click()
-time.sleep(0.5)
-driver.find_element_by_id("react-select-2-option-1").click()
-time.sleep(1)
-submit_button = driver.find_element_by_class_name("styles__Button-sc-15ln7eo-0")
-try:
-    submit_button.click()
-except:
-    pass
+time.sleep(2)
+#driver.find_element_by_id("state").click()
+#time.sleep(0.5)
+#driver.find_element_by_id("react-select-2-option-1").click()
+#time.sleep(1)
+#submit_button = driver.find_element_by_class_name("styles__Button-sc-15ln7eo-0")
+#try:
+#    submit_button.click()
+#except:
+#    pass
 time.sleep(4)
 soup = BeautifulSoup(driver.page_source,'html.parser')
 
@@ -199,7 +211,7 @@ soup = BeautifulSoup(driver.page_source,'html.parser')
 results = soup.findAll(class_='styles__PPUMPrice-sc-12qeqj4-0')
 
 
-for i in range(2):
+for i in range(1):
     resultstr= str(results[i])
     tomate[-1].append(int(resultstr[resultstr.find("$")+1:resultstr.find(".00 x kg")]))
 
@@ -207,7 +219,7 @@ for i in range(2):
 
 #devoto
 tomate.append([])
-URL = 'https://www.devoto.com.uy/frescos/frutas-y-verduras/verduras/tomate?PS=20&sc=3'
+URL = 'https://www.devoto.com.uy/frescos/frutas-y-verduras/verduras/tomate?PS=20&sc=3&O=OrderByNameASC'
 page = requests.get(URL,headers=headers)
 driver.get(URL)
 time.sleep(3)
@@ -223,17 +235,20 @@ soup = BeautifulSoup(driver.page_source,'html.parser')
 
 results = soup.findAll(class_='precio-por-unidad')
 
-del results[3]
-del results[5]
+del results[4]
+del results[1]
 for i in range(len(results)):
     resultstr= str(results[i])
     tomate[-1].append(int(resultstr[resultstr.find("$ <span>")+8:resultstr.find("</span></span>")]))
 
 
 #Naranja Agent
+print ("start naranja")
 naranja=[]
+naranja.append([75,88])
 
 #tiendainglesa
+"""
 naranja.append([])
 URL = 'https://www.tiendainglesa.com.uy/Categoria/Frescos/Frutas/busqueda?0,0,naranja,1894,195,0,rel,%5B%22Otras%22%5D,false,%5B%5D,%5B%5D,'
 driver.get(URL)
@@ -247,7 +262,7 @@ for i in range(len(results)):
     naranja[-1].append(int(resultstr[resultstr.find("$")+1:resultstr.find("$")+5]))
 
 
-
+"""
 #disco
 naranja.append([])
 URL = 'https://www.disco.com.uy/frescos/frutas-y-verduras/frutas/Naranja?map=c,c,c,specificationFilter_3148&sc=4'
@@ -291,16 +306,16 @@ naranja.append([])
 URL = 'https://www.tata.com.uy/buscar?text=naranja&whitelabelRFlag=1'
 #page = requests.get(URL,headers=headers)
 driver.get(URL)
-time.sleep(3)
-driver.find_element_by_id("state").click()
-time.sleep(0.5)
-driver.find_element_by_id("react-select-2-option-1").click()
-time.sleep(1)
-submit_button = driver.find_element_by_class_name("styles__Button-sc-15ln7eo-0")
-try:
-    submit_button.click()
-except:
-    pass
+time.sleep(2)
+#driver.find_element_by_id("state").click()
+#time.sleep(0.5)
+#driver.find_element_by_id("react-select-2-option-1").click()
+#time.sleep(1)
+#submit_button = driver.find_element_by_class_name("styles__Button-sc-15ln7eo-0")
+#try:
+#    submit_button.click()
+#except:
+#    pass
 time.sleep(4)
 soup = BeautifulSoup(driver.page_source,'html.parser')
 
@@ -339,6 +354,7 @@ for i in range(len(results)):
 
 
 #palta Agent
+print ("start palta")
 palta=[]
 
 #tiendainglesa
@@ -398,21 +414,21 @@ palta.append([])
 URL = 'https://www.tata.com.uy/buscar?text=palta'
 #page = requests.get(URL,headers=headers)
 driver.get(URL)
-time.sleep(3)
-driver.find_element_by_id("state").click()
-time.sleep(0.5)
-driver.find_element_by_id("react-select-2-option-1").click()
-time.sleep(1)
-submit_button = driver.find_element_by_class_name("styles__Button-sc-15ln7eo-0")
-try:
-    submit_button.click()
-except:
-    pass
+time.sleep(2)
+#driver.find_element_by_id("state").click()
+#time.sleep(0.5)
+#driver.find_element_by_id("react-select-2-option-1").click()
+#time.sleep(1)
+#submit_button = driver.find_element_by_class_name("styles__Button-sc-15ln7eo-0")
+#try:
+#    submit_button.click()
+#except:
+#    pass
 time.sleep(4)
 soup = BeautifulSoup(driver.page_source,'html.parser')
 
 
-results = soup.findAll(class_='styles__Prices-msqlmx-10')
+results = soup.findAll(class_='fVUXVk')
 
 
 #for i in range(1):
@@ -448,3 +464,19 @@ print ("Naranjas ",naranja)
 print ("Bananas ",banana)
 print ("Tomates ",tomate)
 print ("Palta",palta)
+
+#supermercados = ["Tienda Inglesa","Disco","geant","Tata","devoto"]
+#frutaverdura =["Lugar","Banana", "Tomate", "Naranja", "Palta", "Moron","Palta"]
+
+table.field_names=["Lugar","Banana", "Tomate", "Naranja", "Palta"]
+for i in range (5):
+    table.add_row([supermercados[i],
+    "max = " +str(max(banana[i]))+" med: " + str(round(mean(banana[i]),1)),
+    "max = " +str(max(tomate[i]))+" med: "+ str(round(mean(tomate[i]),1)),
+    "max = " +str(max(naranja[i]))+" med: " + str(round(mean(naranja[i]),1)),
+    "max = " +str(max(palta[i]))+" med: "+ str(round(mean(palta[i]),1))
+    ])
+table_txt = table.get_string()
+with open('output.txt','w') as file:
+    file.write(table_txt)
+print (table)
